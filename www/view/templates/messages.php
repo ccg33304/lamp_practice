09010646e5df3b00f50f2cce40bb545bf26f4f67
ccg33304 <rslb.cssr@gmail.com>
2020-06-02 12:43:10 +0900

    エスケープ用のhprint関数を作成し、全てのprintをhprintに置き換えた

--- www/view/templates/messages.php
+++ www/view/templates/messages.php
@@ -1,6 +1,6 @@
 <?php foreach(get_errors() as $error){ ?>
-  <p class="alert alert-danger"><span><?php print $error; ?></span></p>
+  <p class="alert alert-danger"><span><?php hprint($error); ?></span></p>
 <?php } ?>
 <?php foreach(get_messages() as $message){ ?>
-  <p class="alert alert-success"><span><?php print $message; ?></span></p>
+  <p class="alert alert-success"><span><?php hprint($message); ?></span></p>
 <?php } ?>
